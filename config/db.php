ff5bd0b2babd77862b420fbc016a5ce37f7ab498
DESKTOP-EB9MSBQ\Admin <xht18721626070@gmail.com>
2018-04-16 18:39:42 +0800

    框架初始化

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=rm-uf6y15i3q0drxqr47vo.mysql.rds.aliyuncs.com;dbname=yii2',
+    'username' => 'root',
+    'password' => 'Xht950107',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
